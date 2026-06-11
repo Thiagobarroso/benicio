4e0ac3b7380d6fc4640ca302a23655e7b5508a06
Thiagobarroso <thiagoo.silva@live.com>
2026-06-11 11:09:14 -0300

    atualziação painel

--- script.js
+++ script.js
@@ -71,8 +71,8 @@ async function iniciar() {
 
           ${
             confirmado
-              ? "🟢 Vou"
-              : "🔴 Não Vou"
+              ? "🟢 Presença confirmada"
+              : "🔴 Clique aqui para confirmar"
           }
 
         </button>
